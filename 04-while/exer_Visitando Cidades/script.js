478be1f1287cbbd9cb1f050d430d75f550ef35fc
Henrique Modena <modena3d@gmail.com>
2024-09-19 16:44:59 -0300

    exerc citys

--- 04-while/exer_Visitando Cidades/script.js
+++ 04-while/exer_Visitando Cidades/script.js
@@ -4,3 +4,24 @@
 // e então continue perguntando se o turista visitou alguma outra cidade 
 // até que a resposta seja não. No fim, o programa deve exibir o nome do turista, 
 // quantas e quais cidades ele visitou.
+
+const nome = prompt('Qual é o seu nome? ');
+const visitouCidades = prompt('Você já visitou alguma cidade?\n(S)sim\n(N)não');
+let citys = '';
+
+while (visitouCidades.toUpperCase() === "S") {
+    
+
+    let city = prompt('qual o nome da cidade? ');
+    let visMais = prompt('Você já visitou alguma cidade a mais?\n(S)sim\n(N)não');
+    citys += city + ', ';
+    let cidades = [...citys];
+    
+    if(visMais.toUpperCase() === 'N'){
+        alert(`${nome}, visitou ${cidades.length+1}.\n
+            As cidade visitadas foram: ${citys}`)
+        break;
+    };
+}
+
+
