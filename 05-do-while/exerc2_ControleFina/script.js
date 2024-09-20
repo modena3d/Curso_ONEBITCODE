e6972f151b90070c2ed0d9cfe71e4b1aa49ae748
Henrique Modena <modena3d@gmail.com>
2024-09-20 11:20:10 -0300

    exerc resolvido

--- 05-do-while/exerc2_ControleFina/script.js
+++ 05-do-while/exerc2_ControleFina/script.js
@@ -8,3 +8,32 @@
 // menu com a quantidade de dinheiro e as opções. A opção de remover dinheiro 
 // deve fazer o mesmo, porém subtraindo o valor.
 //  A opção de sair deve encerrar o programa.
+
+let dinheiroDisponivel = prompt('Digite a quantidade de dinheiro disponivel:')
+let menu1 = '';
+
+do {
+    menu1 = prompt(`Escolha uma das opções:\n` +
+        `Total disponível: R$${dinheiroDisponivel}\n` +
+        `1 - Adicionar dinheiro\n` +
+        `2 - Remover dinheiro\n` +
+        `3 - Sair`);
+
+    switch (menu1) {
+        case '1':
+            let addMoney = prompt(`Digite a quantia a ser adicionada: `);
+            dinheiroDisponivel = Number(dinheiroDisponivel);
+            dinheiroDisponivel += Number(addMoney);
+            break;
+        case '2':
+            let subMoney = prompt(`Digite a quantia a ser retirada: `);
+            dinheiroDisponivel = Number(dinheiroDisponivel);
+            dinheiroDisponivel -= Number(subMoney);
+            break;
+        case '3':
+            alert('O sistema está sendo encerrado...')
+            break;
+        default:
+            break;
+    }
+} while (menu1 !== '3');
