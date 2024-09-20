5e691ac2275b9fc54dd3dde8189517dd7bbf205e
Henrique Modena <modena3d@gmail.com>
2024-09-20 11:46:22 -0300

    result tab

---
+++ 06-For/exerc_tabuada/script.js
@@ -0,0 +1,21 @@
+// Robô da Tabuada
+// Escreva um programa em javascript 
+// que seja capaz de calcular a tabuada 
+// de um determinado número (de 1 a 20). 
+// O programa deve começar pedindo o número 
+// a ser usado nos cálculos e então salvar 
+// todos os resultados de multiplicações de 1 a 20
+//  usando o número informado dentro de uma 
+//  variável do tipo string. Depois o programa 
+//  deve exibir esses resultados e finalizar.
+
+const x = prompt('Digite o numero a ser multiplicado');
+let result ='';
+
+for (let i = 1; i <= 20; i++) {
+    result += '-> '+ x + ' X ' + i + ' = '+ (x * i) + '\n';    
+};
+
+alert(`Resultado da Tabuada de numero ${x} é de:\n
+    ${result}`);
+
