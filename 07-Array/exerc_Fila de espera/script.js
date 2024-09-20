25b8b31566b6a93a1d5e44cd4a947cf1f4caa7ab
Henrique Modena <modena3d@gmail.com>
2024-09-20 18:00:18 -0300

    final exer fila

--- 07-Array/exerc_Fila de espera/script.js
+++ 07-Array/exerc_Fila de espera/script.js
@@ -16,3 +16,43 @@
 //  paciente consultado, e “Sair”. O programa só 
 //  deve ser encerrado ao escolher a opção de “Sair”, 
 //  caso contrário deve voltar ao menu.
+
+let filaDeEspera = ['Joao', 'Maria', 'Marcos', 'Jose'];
+let display = '';
+
+
+do {   
+    let pacientes = '';
+    for (let i = 0; i < filaDeEspera.length ; i++) {
+        pacientes += (i + 1) + 'º ' + `${filaDeEspera[i]}` + `\n`;
+    }
+
+    display = prompt('Lista de Espera:\n' +
+    `${pacientes}\n` +
+    `Escolha umas das opção a baixo:\n` +
+    `1 - Novo paciente\n` +
+    `2 - Consultar paciente\n` +
+    `3 - Sair`
+)
+    
+    switch (display) {
+        case "1":
+            const novoPaciente = prompt('Digite o nome do paciente:');
+            filaDeEspera.push(novoPaciente);
+            break;
+        case "2":
+            const pacienteConsultado = filaDeEspera.shift()
+            if(!pacienteConsultado){
+                alert("Não há pacientes na fila!")
+            }else{
+                alert(pacienteConsultado + ' foi removido')
+            }
+    
+            break;
+        case "3":
+    
+            break;
+        default:
+            break;
+    }
+} while (display !== "3");
