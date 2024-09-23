36929d43da0fc65846ae4a583150c24717ccd97c
Henrique Modena <modena3d@gmail.com>
2024-09-23 14:41:07 -0300

    exerc finalizado

--- 07-Array/exer PilhaCartas/script.js
+++ 07-Array/exer PilhaCartas/script.js
@@ -8,3 +8,30 @@
 //   deve retirar a carta do topo do baralho e mostrar na tela o nome da carta puxada.
 //    O programa só deve ser encerrado ao escolher a opção de “Sair”, 
 //    caso contrário deve voltar ao menu.
+let cards = [];
+let menu1 = '';
+do {
+    
+        menu1 = prompt(`Cartas no baralho: ${cards.length}\n` +
+        '1. Adicionar uma carta\n' +
+        '2. Puxar uma carta\n' +
+        '3. Sair'
+    );
+    
+    switch (menu1) {
+        case '1':
+            let addCard = prompt('Qual é a carta?');
+            cards.push(addCard);
+            console.log(cards);  
+            break;
+        case '2':
+            let removeCards = cards.pop();
+            alert(`Você puxou a ${removeCards}`);
+            break;
+        case '3':
+            alert('Saindo...')
+            break;    
+        default:
+            alert('Opção inválida!');
+    }
+} while (menu1 !== '3');
