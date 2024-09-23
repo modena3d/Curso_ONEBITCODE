76a9706846a59d77d635024069957ff05a714d50
Henrique Modena <modena3d@gmail.com>
2024-09-23 16:15:30 -0300

    cadast_imoveis

---
+++ 09-CadastroImoveis/script.js
@@ -0,0 +1,12 @@
+// Cadastro de Imóveis
+// Escreva um programa em javascript que funcione como um cadastro de imóveis e atenda aos seguintes requisitos:
+
+// 1-Deve ter um menu interativo que sempre é exibido até que o usuário escolha sair.
+// 2-O menu interativo deve mostrar no topo a quantidade de imóveis cadastrados.
+// 3-O menu deve ter a opção de salvar um imóvel.
+// 4-Para salvar um novo imóvel o programa deve pedir as seguintes informações: Nome do proprietário.Quantidade de quartos.Quantidade de banheiros.Se possui garagem.
+// 5-Nome do proprietário.
+// 6-Quantidade de quartos.
+// 7-Quantidade de banheiros.
+// 8-Se possui garagem.
+// 9-O menu também deve ter a opção de mostrar todos os imóveis salvos.
