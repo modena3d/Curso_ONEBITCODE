ab67d6e1466a5145e92028f5af181bc3fd98155b
Henrique Modena <modena3d@gmail.com>
2024-09-23 16:12:36 -0300

    ok

---
+++ 08-ArrayBim/script.js
@@ -0,0 +1,10 @@
+// Arrays podem receber qualquer tipo da dado, inclusive outros arrays
+const arr = [
+    "1º Nível",
+    ["2º nível", 42, true],
+    [
+      ["3º nível, 1º item", "Olá, mundo!"],
+      ["3º nível, 2º item", "Oi, mundo!"],
+    ],
+    []
+  ]
