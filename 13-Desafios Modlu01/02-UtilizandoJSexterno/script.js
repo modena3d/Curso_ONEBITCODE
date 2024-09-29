6766394c791de2bed538b5815dbe2182e962c3a7
modenapclocal <modena3d@gmail.com>
2024-09-28 22:16:46 -0300

    difere de idade

--- 13-Desafios Modlu01/02-UtilizandoJSexterno/script.js
+++ 13-Desafios Modlu01/02-UtilizandoJSexterno/script.js
@@ -7,3 +7,16 @@
 // 6 Calcular a diferença de idade
 // 7 Exibir o nome da pessoa mais velha e sua idade
 // 8 Exibir o nome da pessoa mais nova e sua idade 9 Exibir a diferença de idade
+
+const idadeVelha = prompt('Digite a idade de Pessoa mais velha:')
+const nomeVelha = prompt('Digite o nome da Pessoa mais velha:')
+const idadeNova = prompt('Digite a idade de Pessoa mais nova:')
+const nomeNova = prompt('Digite o nome da Pessoa mais nova:')
+
+const diferencaDeIdade = idadeVelha - idadeNova;
+
+alert(`${nomeVelha}, tem ${idadeVelha} anos\n` +
+  `${nomeNova}, tem ${idadeNova} anos\n` +
+  `A diferença de idade entre ambos são de ${diferencaDeIdade} anos`
+)
+
