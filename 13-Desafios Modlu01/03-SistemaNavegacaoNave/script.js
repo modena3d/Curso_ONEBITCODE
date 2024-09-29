88c0655870d29cc71bf7bd035984f542b8628c3b
modenapclocal <modena3d@gmail.com>
2024-09-28 22:22:22 -0300

    nave

---
+++ 13-Desafios Modlu01/03-SistemaNavegacaoNave/script.js
@@ -0,0 +1,9 @@
+
+// Utilizando Condicionais
+// O CÓDIGO DEVE ESTAR NUM ARQUIVO SEPARADO E IMPORTADO NUMA PÁGINA HTML
+// Neste exercício vamos simular o sistema de navegação de uma nave interagindo com o usuário.
+// O sistema deve fazer o seguinte:
+// 1 Perguntar o nome do piloto
+// 2 Colocar a velocidade inicial da nave como 0
+// 3 Perguntar a que velocidade ele gostaria de acelerar a nave 4 Pedir uma confirmação informando que está indo para velocidade X km/s.
+// • Se quiser, atualizar a velocidade.
