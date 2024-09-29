2c8432c837143d727c30796e993f17f401543d44
modenapclocal <modena3d@gmail.com>
2024-09-28 23:04:30 -0300

    unidade luz

--- 13-Desafios Modlu01/04-Unidade Espaciais/script.js
+++ 13-Desafios Modlu01/04-Unidade Espaciais/script.js
@@ -4,3 +4,32 @@
 // Neste exercício faremos um conversor de distâncias espaciais.
 // ▪ O objetivo é colocarmos uma distância em
 // anos-luz (l.y.) e converter para uma unidade desejada.
+// ■ O conversor deve:
+// 1 Perguntar a distância em anos-luz
+
+// 2 Oferecer uma lista de opções.com
+//  Parsec (pc)
+//  Unidade astronônima (AU)
+//  Quilômetros (km)
+
+// 3 Perguntar qual operação deseja realizar
+//  A resposta receberá o número da opção escolhida
+
+// 4. Utilize o switch para verificar a opção escolhida pelo usuário
+//  Armazenar o nome da unidade
+//  Armazenar o valor convertido de anos-luz para a unidade desejada
+
+// 5 Exiba com alerta contendo a distância em anos-luz e a distância convertida para a operação desejada
+//  Distância em Anos-luz: <distancia digitada em anos-luz> <Conversão desejada>: <distància convertida>
+
+// 6 Utiliza as seguintes conversões
+// 11.y.= 0,306601pc
+// 1 l.y. = 63241,1 AU
+// 11.y.=9,5*1012
+
+// 7 Utilize a biblioteca Math para a conversão de anos-luz para quilômetros.
+// Utilize Math.pow(10, 12) para obter o valor de 1012 na conversão de anos-luz para quilômetro
+
+// 8 Caso o usuário digite uma opção que são seja uma das três, em vez do nome da distâcia e da conversão, deve ser exibido:
+// Distância em Anos-luz: <distancia digitada em anos-luz>
+// Unidade não identificada: Conversão fora do escopo
