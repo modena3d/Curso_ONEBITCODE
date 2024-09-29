def7e9399a3a40161392a1f3f93abacdb6932c4e
modenapclocal <modena3d@gmail.com>
2024-09-29 02:17:05 -0300

    dobra espacial

---
+++ 13-Desafios Modlu01/05-Nave while/script.js
@@ -0,0 +1,19 @@
+
+// Utilizando o While
+// O CÓDIGO DEVE ESTAR NUM ARQUIVO SEPARADO E IMPORTADO NUMA PÁGINA HTML
+// Neste exercício vamos simular o sistema de navegação de uma nave interagindo com o usuário.
+// Para este problema, precisamos:
+// 1 Pedir nome da nave
+// 2 Perguntar se deseja entrar na dobra com o seguinte mensagem:
+// Deseja entrar em dobra espacial?
+// 1- Sim
+// 2- Não
+// Se a resposta for sim, contabilizaremos uma dobra
+// 3 O usuário deve ser perguntado se deseja realizar a próxima dobra
+// Deseja realizar a próxima dobra?
+// 1- Sim
+// 2- Não
+// Para cada "Sim", vamos contabilizar mais uma dobra realizada;
+// 4 A pergunta deve ser feita novamente, até que escolha seja “Não” 5 Ao escolher "Não” em qualquer pergunta, a contagem deve ser encerrada;
+// 6 O programa deve informar o nome da nave e o número de vezes seguidos que a dobra espacial foi realizada
+
