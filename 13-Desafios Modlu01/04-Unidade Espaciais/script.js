62e4398c4fa6a974d2ed3a7a34aab3702f525539
modenapclocal <modena3d@gmail.com>
2024-09-28 22:56:13 -0300

    unidade espaciais

---
+++ 13-Desafios Modlu01/04-Unidade Espaciais/script.js
@@ -0,0 +1,6 @@
+
+// Fixando o uso do Switch
+// O CÓDIGO DEVE ESTAR NUM ARQUIVO SEPARADO E IMPORTADO NUMA PÁGINA HTML
+// Neste exercício faremos um conversor de distâncias espaciais.
+// ▪ O objetivo é colocarmos uma distância em
+// anos-luz (l.y.) e converter para uma unidade desejada.
