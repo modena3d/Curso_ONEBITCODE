e5b6502a5b05bf7a473b5ea0d77e7956f20a312d
Henrique Modena <modena3d@gmail.com>
2024-09-30 16:19:53 -0300

    ok

--- 13-Desafios Modlu01/11-objetos/script.js
+++ 13-Desafios Modlu01/11-objetos/script.js
@@ -1,28 +1,42 @@
 
 // Praticando Objetos
 // O CÓDIGO DEVE ESTAR NUM ARQUIVO SEPARADO E IMPORTADO NUMA PÁGINA HTML
+
 // Faremos um aplicativo que fará o mapeamento de velocidade de uma espaçonave.
+
 // Neste aplicativo, vamos cadastrar uma espaçonave com seu nome, 
 
-// tipo e a velocidade máxima que esta nave pode atingir para que não entre 
+// tipo 
+// e a velocidade máxima que esta nave pode atingir para que não entre 
 // em combustão.
+
 // 1 Nave deve partir da velocidade 0 km/s
+
 // 2 Menu deve ser exibido para o usuário perguntando se ele quer acelerar ou parar 
 // a nave.
+
 // •Se ele quiser parar, uma mensagem deve ser exibida com as características da nave 
 // (nome e tipo) e em que velocidade ele
 // estava.
+
 // • Se ele quiser acelerar, deve ser exibido um prompt para que ele informe quanto 
 // ele quer acelerar e este valor será incrementado na velocidade.
+
 // 3 Quando a velocidade for incrementada, deve-ser fazer. uma verificação para ver se a 
 // espaçonave está acima da velocidade máxima dela.
+
 // Se ela estiver, uma mensagem deve ser exibida informando a velocidade da nave e a 
 // velocidade máxima permitida.
+
 // Para resolver estes problemas precisaremos:
 // • Exibir um prompt para o nome da nave
+
 // Exibir um prompt para o tipo da nave
+
 // • Exibir um prompt para velocidade máxima desta nave
-// • Exibir um prompt perguntando se o usuário quer acelerar ou parar ▪ Se ele quiser parar, vamos exibir a mensagem final
-// •Se ele quiser acelerar, precisamos de um prompt para que o usuário informe o quanto quer acelerar
+
+// • Exibir um prompt perguntando se o usuário quer acelerar ou parar 
+// ▪ Se ele quiser parar, vamos exibir a mensagem final
+// • Se ele quiser acelerar, precisamos de um prompt para que o usuário informe o quanto quer acelerar
 // • Exibir uma mensagem de alerta quando a velocidade máxima for ultrapassada
 // • Criar um objeto para a nave
