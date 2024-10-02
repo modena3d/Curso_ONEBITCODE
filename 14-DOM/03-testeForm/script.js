9d5899c42b85896bc93747aa1c7060ca65305337
Henrique Modena <modena3d@gmail.com>
2024-10-02 10:49:19 -0300

    br

--- 14-DOM/03-testeForm/script.js
+++ 14-DOM/03-testeForm/script.js
@@ -19,3 +19,56 @@
 
 // Para esse exercício você deve utilizar apenas eventos adicionados via javascript
 //  e a página deve funcionar sem acionar um recarregamento.
+
+const btnAdd = document.getElementById('btnAddNewTec')
+const ulList = document.getElementById('ulList')
+// console.log(list);
+let id = 0;
+
+  btnAdd.addEventListener('click',function(){
+    const li = document.createElement('li')
+    li.id = 'id-' + id++
+    li.innerText = 'Nome: '
+    ulList.appendChild(li)
+    const inputLinguagens =  document.createElement('input')
+    inputLinguagens.id = li.id
+    li.appendChild(inputLinguagens)
+    const btnRadio1 = document.createElement('input')
+    const LbParagraf1 = document.createElement('label')
+    const LabelRadio1 = document.createElement('label')
+    LbParagraf1.innerText =' Experiência: '
+    LabelRadio1.innerText = '0-2 anos'
+    btnRadio1.type = 'radio'
+    btnRadio1.name = 'xp'
+    LabelRadio1.htmlFor = 'xp1'
+    btnRadio1.id = 'xp1'
+    li.appendChild(LbParagraf1)
+    li.appendChild(btnRadio1)
+    li.appendChild(LabelRadio1)
+    const btnRadio2 = document.createElement('input')
+    const LabelRadio2 = document.createElement('label')
+    LabelRadio2.innerText = '3-4 anos'
+    btnRadio2.type = 'radio'
+    btnRadio2.name = 'xp'
+    LabelRadio2.htmlFor = 'xp2'
+    btnRadio2.id = 'xp2'
+    li.appendChild(btnRadio2)
+    li.appendChild(LabelRadio2)
+    const btnRadio3 = document.createElement('input')
+    const LabelRadio3 = document.createElement('label')
+    LabelRadio3.innerText = '5+ anos '
+    btnRadio3.type = 'radio'
+    btnRadio3.name = 'xp'
+    LabelRadio3.htmlFor = 'xp3'
+    btnRadio3.id = 'xp3'
+    li.appendChild(btnRadio3)
+    li.appendChild(LabelRadio3)
+    const btnRemover = document.createElement('button')
+    btnRemover.innerText = 'Remover'
+    btnRemover.id = 'btnDel_' + id
+    li.appendChild(btnRemover)
+    const br = document.createElement('br')
+    br.innerHTML = '<br>'
+    li.appendChild(br)
+    
+  })
