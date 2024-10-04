e814afcbabb11d953648784c789ec1dfa9dd561a
EmpresaFict <modena3d@gmail.com>
2024-10-03 22:12:25 -0300

    check ok

--- 14-DOM/03-testeForm/script.js
+++ 14-DOM/03-testeForm/script.js
@@ -2,146 +2,161 @@
 
 // ## Cadastro de Devs
 
-// Construa uma página web que permita cadastrar desenvolvedores, suas 
-// tecnologias e tempos de experiência. 
+// Construa uma página web que permita cadastrar desenvolvedores, suas
+// tecnologias e tempos de experiência.
 
-// - A página deve ter um input para o nome completo do desenvolvedor e um botão 
+// - A página deve ter um input para o nome completo do desenvolvedor e um botão
 // para adicionar uma nova tecnologia.
 // - Ao clicar no botão um evento deve ser disparado criando no formulário uma nova
 // linha de inputs contendo:
 //     - um campo de texto para o nome da tecnologia
-//     - um grupo de botões tipo “radio” com opções de tempo de experiência 
+//     - um grupo de botões tipo “radio” com opções de tempo de experiência
 //     (por exemplo, “0-2 anos”, “3-4 anos” e “5+ anos”)
 //     - um botão de remover que deve excluir essa linha.
-// - O formulário também deve ter um botão de cadastrar que salva as informações 
-// do dev em um array e limpa o formulário. Para testar o funcionamento você pode 
+// - O formulário também deve ter um botão de cadastrar que salva as informações
+// do dev em um array e limpa o formulário. Para testar o funcionamento você pode
 // utilizar um simples console.log()
 
 // Para esse exercício você deve utilizar apenas eventos adicionados via javascript
 //  e a página deve funcionar sem acionar um recarregamento.
 
-const btnAdd = document.getElementById('btnAddNewTec')
-const ulList = document.getElementById('ulList')
+const btnAdd = document.getElementById("btnAddNewTec");
+const ulList = document.getElementById("ulList");
 let developers = [];
-let tecs = []
+let tecs = [];
 // console.log(list);
 let id = 0;
 
-btnAdd.addEventListener('click', function () {
-  let nome = document.getElementById('nome').value
+btnAdd.addEventListener("click", function () {
+  let nome = document.getElementById("nome").value;
   // console.log(nome);
 
-  const li = document.createElement('li')
-  li.id = 'id-' + id++
-  li.innerText = 'Nome: '
-  ulList.appendChild(li)
-  const inputLinguagens = document.createElement('input')
-  inputLinguagens.id = li.id
-  li.appendChild(inputLinguagens)
-  const btnRadio1 = document.createElement('input')
-  const LbParagraf1 = document.createElement('label')
-  const LabelRadio1 = document.createElement('label')
-  LbParagraf1.innerText = ' Experiência: '
-  LabelRadio1.innerText = '0-2 anos'
-  btnRadio1.type = 'radio'
-  btnRadio1.name = 'xp' + id
-  LabelRadio1.htmlFor = 'xp1_' + id
-  btnRadio1.id = 'xp1_' + id
-  li.appendChild(LbParagraf1)
-  li.appendChild(btnRadio1)
-  li.appendChild(LabelRadio1)
-  const btnRadio2 = document.createElement('input')
-  const LabelRadio2 = document.createElement('label')
-  LabelRadio2.innerText = '3-4 anos'
-  btnRadio2.type = 'radio'
-  btnRadio2.name = 'xp' + id
-  LabelRadio2.htmlFor = 'xp2_' + id
-  btnRadio2.id = 'xp2_' + id
-  li.appendChild(btnRadio2)
-  li.appendChild(LabelRadio2)
-  const btnRadio3 = document.createElement('input')
-  const LabelRadio3 = document.createElement('label')
-  LabelRadio3.innerText = '5+ anos '
-  btnRadio3.type = 'radio'
-  btnRadio3.name = 'xp' + id
-  LabelRadio3.htmlFor = 'xp3_' + id
-  btnRadio3.id = 'xp3_' + id
-  li.appendChild(btnRadio3)
-  li.appendChild(LabelRadio3)
-  const btnRemover = document.createElement('button')
-  btnRemover.innerText = 'Remover'
-  btnRemover.id = 'btnDel_'
-  btnRemover.addEventListener('click', function () {
-    li.remove()
+  const li = document.createElement("li");
+  li.id = "id-" + id++;
+  li.innerText = "Nome: ";
+  ulList.appendChild(li);
+  const inputLinguagens = document.createElement("input");
+  inputLinguagens.id = li.id;
+  li.appendChild(inputLinguagens);
+  const btnRadio1 = document.createElement("input");
+  const LbParagraf1 = document.createElement("label");
+  const LabelRadio1 = document.createElement("label");
+  LbParagraf1.innerText = " Experiência: ";
+  LabelRadio1.innerText = "0-2 anos";
+  btnRadio1.type = "radio";
+  btnRadio1.name = "xp" + id;
+  LabelRadio1.htmlFor = "xp1_" + id;
+  btnRadio1.id = "xp1_" + id;
+  li.appendChild(LbParagraf1);
+  li.appendChild(btnRadio1);
+  li.appendChild(LabelRadio1);
+  const btnRadio2 = document.createElement("input");
+  const LabelRadio2 = document.createElement("label");
+  LabelRadio2.innerText = "3-4 anos";
+  btnRadio2.type = "radio";
+  btnRadio2.name = "xp" + id;
+  LabelRadio2.htmlFor = "xp2_" + id;
+  btnRadio2.id = "xp2_" + id;
+  li.appendChild(btnRadio2);
+  li.appendChild(LabelRadio2);
+  const btnRadio3 = document.createElement("input");
+  const LabelRadio3 = document.createElement("label");
+  LabelRadio3.innerText = "5+ anos ";
+  btnRadio3.type = "radio";
+  btnRadio3.name = "xp" + id;
+  LabelRadio3.htmlFor = "xp3_" + id;
+  btnRadio3.id = "xp3_" + id;
+  li.appendChild(btnRadio3);
+  li.appendChild(LabelRadio3);
+  const btnRemover = document.createElement("button");
+  btnRemover.innerText = "Remover";
+  btnRemover.id = "btnDel_";
+  btnRemover.addEventListener("click", function () {
+    li.remove();
     if (tecs.length > 0) {
-      tecs.pop()
+      tecs.pop();
       console.log(tecs);
-
     } else {
       console.log(tecs.length);
-
     }
-  })
-  li.appendChild(btnRemover)
-  const br = document.createElement('br')
-  br.innerHTML = '<br>'
-  li.appendChild(br)
+  });
+  li.appendChild(btnRemover);
+  const br = document.createElement("br");
+  br.innerHTML = "<br>";
+  li.appendChild(br);
   // developers.push(li)
+});
 
-})
-
-
-const btnCadastrar = document.getElementById('btnCadastrar')
-btnCadastrar.addEventListener('click', function (eve) {
-  eve.preventDefault()
-  let dev = {}
-  dev.tecs = []
-  const nome = document.getElementById('nome').value
+const btnCadastrar = document.getElementById("btnCadastrar");
+btnCadastrar.addEventListener("click", function (eve) {
+  eve.preventDefault();
+  let dev = {};
+  dev.tecs = [];
+  const nome = document.getElementById("nome").value;
   let count = 0;
   let tecsSize;
+  let groupRadio;
+  const listTecs = document.querySelectorAll("li");
+  listTecs.forEach((li) => {
+    dev.tecs.push(li.firstElementChild.value);
+    groupRadio = Array.from(li.querySelectorAll('li input[type="radio"]'));
+  });
 
-  const listTecs = document.querySelectorAll('li')
-  listTecs.forEach(li => {
-    dev.tecs.push(li.firstElementChild.value)
-  })
   if (nome.length > 0) {
-    // console.log('?: ', developers[developers.length-1].tecs.length, 'end'); //////////////BUG
-    dev.nome = nome
-    dev.tecs[count++]
-    // console.log(developers[developers.length-1].tecs.length);  
-    developers.push(dev)
-    tecsSize = developers[developers.length-1].tecs
-    console.log(tecsSize);//////
-    const li = document.getElementsByTagName('li')
-    
-    if ( li.length > 0) {
+    dev.nome = nome;
+    dev.tecs[count++];
+    dev.xp = developers.push(dev);
+    tecsSize = developers[developers.length - 1].tecs;
+    radioSize = developers[developers.length - 1];
+    // console.log(`RADIO...`, radioSize);
+
+    const li = document.getElementsByTagName("li");
+
+    if (li.length > 0) {
+      ///------Tecs--------
       for (let i = 0; i < tecsSize.length; i++) {
-      console.log('tec:', tecsSize[i]);
-      if (tecsSize[i].length === 0) {
-        alert('Por favor, preencha o Nome da tecnologia...')
+        console.log("tec:", tecsSize[i]);
+        if (tecsSize[i].length === 0) {
+          alert("Por favor, preencha o Nome da tecnologia...");
+          break;
+        }
       }
-    }
-    const checks = document.querySelectorAll('input[type="radio"]')
-    console.log(checks);
-    checks.forEach((check)=>{
-      check.checked
-    })
 
-
+      listTecs.forEach((li) => {
+        groupRadio = Array.from(li.querySelectorAll('li input[type="radio"]'));
+        // console.log(groupRadio);
+        const isAnyChecked = groupRadio.some((radio) => radio.checked);
+        if (!isAnyChecked) {
+          const inputName = li.getElementsByTagName("input");
+          alert(
+            `Selecione o tempo de experiência da(s) tecnologia(s): ${inputName[0].value}`
+          );
+        }
+      });
+      //-----------------------
+      // let radioInputs;
+      // for (let i = 0; i < li.length; i++) {
+      //   // console.log(`LI: `, li[i].getElementsByTagName("input"));
+      //   const inputs = li[i].getElementsByTagName("input");
+      //   radioInputs = Array.from(inputs).filter(
+      //     (input) => input.type === "radio"
+      //   );
+      // }
+      // console.log("radioInp: ", radioInputs);
+      // const isAnyChecked = radioInputs.some((radio) => radio.checked);
+      // if (!isAnyChecked) {
+      //   alert(`Selecione o tempo de experiência da(s) tecnologia(s): `);
+      // }
     } else {
-      alert('Por favor, adicione uma experiencia...')
+      alert("Por favor, adicione uma experiencia...");
     }
   } else {
-    alert('Por favor, preencha o Nome Completo...')
+    alert("Por favor, preencha o Nome Completo...");
   }
-  
 
-  
   // console.log(tecs);
 
   // console.log(nome);
-})
-
-
+});
 
 // developers = [dev1{nome: , tec: [] ,} , ]
