5f19794b3b07ac63b6bad5f2745d630567f03bdd
modenapclocal <modena3d@gmail.com>
2024-10-05 13:13:41 -0300

    ok

---
+++ 14-DOM/03-testeForm/script3.js
@@ -0,0 +1,18 @@
+function createLabel(text, htmlFor) {
+  const label = document.createElement('label')
+  label.htmlFor = htmlFor
+  label.innerText = text
+  return label
+}
+
+function createInput(id, value, name, type = 'text', placeholder = '') {
+  const input = document.createElement('inpt')
+  input.id = id
+  input.value = value
+  input.name = name
+  input.type = type
+  input.placeholder = placeholder
+  return input
+}
+
+const addTechBtn = document.getElementById('addTechBtn')
