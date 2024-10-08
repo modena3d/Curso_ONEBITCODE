3f72473705569a414704260cf3c7a966dc9b5a96
Henrique Modena <modena3d@gmail.com>
2024-10-08 11:21:51 -0300

    ok

--- 14-DOM/07-Jogo_Da_Velha/scriptNew.js
+++ 14-DOM/07-Jogo_Da_Velha/scriptNew.js
@@ -5,6 +5,9 @@ const board = document.querySelector(".board");
 let clickedSquare = null;
 let playerName_1 = ''
 let playerName_2 = ''
+let squaresPlayer1 = []
+let squaresPlayer2 = []
+
 document
   .getElementById("btnPlay")
   .addEventListener("click", function (clickBtnPlay) {
@@ -14,24 +17,19 @@ document
   });
 
 function startGame() {
-  console.log("BORD: ", board.classList.value);
   let playGame = checkNameInputField();
   if (playGame) {
     activeSquareClick();
-    checkTimePlay();
     btnPlayResetGame();
   }
 }
 function checkTimePlay() {
-  console.log(`Quadrado clicado:`, clickedSquare);
   if (board.classList.contains("playerName_1")) {
-    console.log(board.classList.value);
     board.classList.remove("playerName_1");
     board.classList.add("playerName_2");
     displayMesage.innerText = `Vez do ${playerName_2}: (O) jogar`;
     return playerName_2;
   } else {
-    console.log(board.classList.value);
     board.classList.remove("playerName_2");
     board.classList.add("playerName_1");
     displayMesage.innerText = `Vez do ${playerName_1}: (X) jogar`;
@@ -61,7 +59,6 @@ function btnPlayResetGame() {
   if (textBtn == "Play") {
     btnPlay.innerText = "Reset";
     board.classList.add("playerName_1");
-    console.log("PLAY");
     return true;
   } else {
     btnPlay.innerText = "Play";
@@ -74,7 +71,8 @@ function btnPlayResetGame() {
     document.querySelectorAll(".square").forEach((square) => {
       square.innerText = "";
     });
-    console.log("RESET");
+    squaresPlayer1 = []
+    squaresPlayer2 = []
     return false;
   }
 }
@@ -92,8 +90,93 @@ function disableSquareClick() {
 function handleSquareClick(ev) {
   const squareNumber = ev.target.dataset.number;
   clickedSquare = squareNumber;
-  console.log(`Quadrado clicado:`, squareNumber);
   ev.target.removeEventListener("click", handleSquareClick);
+  let playerTime = checkTimePlay();
+  add_X_or_O_toTheSquare(playerTime)
 }
 
+function add_X_or_O_toTheSquare(playerTime) {
+  let id = Number(clickedSquare)
+  console.log(id);
+  if (playerTime == playerName_1) {
+    const square = document.querySelector(`[data-number="${id}"]`)
+    square.innerText = 'O'
+    squaresPlayer2.push(id)
+  } else {
+    const square = document.querySelector(`[data-number="${id}"]`)
+    square.innerText = 'X'
+    squaresPlayer1.push(id)
+  }
+  sequenceChecker(squaresPlayer1, squaresPlayer2)
+}
 
+function sequenceChecker(squaresPlayer1, squaresPlayer2) {
+  console.log('Sq1: ', squaresPlayer1);
+  console.log('Sq2: ', squaresPlayer2);
+  //123
+  const sequence_1 = [1, 2, 3]
+  const p1Sq1 = sequence_1.every(num => squaresPlayer1.includes(num));
+  const p2Sq1 = sequence_1.every(num => squaresPlayer2.includes(num));
+  //456
+  const sequence_2 = [4, 5, 6]
+  const p1Sq2 = sequence_2.every(num => squaresPlayer1.includes(num));
+  const p2Sq2 = sequence_2.every(num => squaresPlayer2.includes(num));
+  //789
+  const sequence_3 = [7, 8, 9]
+  const p1Sq3 = sequence_3.every(num => squaresPlayer1.includes(num));
+  const p2Sq3 = sequence_3.every(num => squaresPlayer2.includes(num));
+  //147
+  const sequence_4 = [1, 4, 7]
+  const p1Sq4 = sequence_4.every(num => squaresPlayer1.includes(num));
+  const p2Sq4 = sequence_4.every(num => squaresPlayer2.includes(num));
+  //258
+  const sequence_5 = [2, 5, 8]
+  const p1Sq5 = sequence_5.every(num => squaresPlayer1.includes(num));
+  const p2Sq5 = sequence_5.every(num => squaresPlayer2.includes(num));
+  //369
+  const sequence_6 = [3, 6, 9]
+  const p1Sq6 = sequence_6.every(num => squaresPlayer1.includes(num));
+  const p2Sq6 = sequence_6.every(num => squaresPlayer2.includes(num));
+  //159
+  const sequence_7 = [1, 5, 9]
+  const p1Sq7 = sequence_7.every(num => squaresPlayer1.includes(num));
+  const p2Sq7 = sequence_7.every(num => squaresPlayer2.includes(num));
+  //357
+  const sequence_8 = [3, 5, 7]
+  const p1Sq8 = sequence_8.every(num => squaresPlayer1.includes(num));
+  const p2Sq8 = sequence_8.every(num => squaresPlayer2.includes(num));
+
+  let checkWinnerPlayer_1 = {
+    p1Sq1,
+    p1Sq2,
+    p1Sq3,
+    p1Sq4,
+    p1Sq5,
+    p1Sq6,
+    p1Sq7,
+    p1Sq8
+  }
+  let checkWinnerPlayer_2 = {
+    p2Sq1,
+    p2Sq2,
+    p2Sq3,
+    p2Sq4,
+    p2Sq5,
+    p2Sq6,
+    p2Sq7,
+    p2Sq8
+  }
+  paintSquare(checkWinnerPlayer_1, checkWinnerPlayer_2)
+}
+
+function paintSquare(checkWinnerPlayer_1, checkWinnerPlayer_2) {
+  const p1 = checkWinnerPlayer_1
+  const p2 = checkWinnerPlayer_2
+  if (p1.p1Sq1 || p1.p1Sq2 || p1.p1Sq3 || p1.p1Sq4 || p1.p1Sq5 || p1.p1Sq6 || p1.p1Sq7 || p1.p1Sq8) {
+    console.log('P1 WIN');
+  }
+  if (p2.p2Sq1 || p2.p2Sq2 || p2.p2Sq3 || p2.p2Sq4 || p2.p2Sq5 || p2.p2Sq6 || p2.p2Sq7 || p2.p2Sq8) {
+    console.log('P2 WIN');
+  }
+
+}
