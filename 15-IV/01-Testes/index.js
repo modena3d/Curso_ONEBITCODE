2f4188bf03fbfdab432800a097d786f5d16444a3
Henrique Modena <modena3d@gmail.com>
2024-10-08 18:20:08 -0300

    testes ok

---
+++ 15-IV/01-Testes/index.js
@@ -0,0 +1,12 @@
+const coisas = {
+    frutas: ['pera', 'uva', 'maca', 'banana', 'abacaxi'],
+    numbers: [10, 50, 20, 5, 42, 8, 60],
+    booleanArray: [false, false, true, false, true, false, true]
+}
+
+const filtrados = coisas.numbers.filter(valor => valor > 40)
+console.log(filtrados);
+
+const numbers = [...coisas.frutas[4]].filter(vl => vl === 'a').map(vl => vl)
+console.log(numbers);
+console.log(coisas.numbers);
