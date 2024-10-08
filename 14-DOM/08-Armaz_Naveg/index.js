fff5124aaf5d1eb3494f64520527dcfe93b68600
Henrique Modena <modena3d@gmail.com>
2024-10-08 16:15:38 -0300

    fim armaz e fim dom

--- 14-DOM/08-Armaz_Naveg/index.js
+++ 14-DOM/08-Armaz_Naveg/index.js
@@ -1,5 +1,39 @@
-document.getElementById('sessionBtn').addEventListener('click', function (ev) {
+document.getElementById('sessionBtn').addEventListener('click', function () {
+    const input = document.getElementById('session')
+    // window.sessionStorage
+    sessionStorage.setItem('info', input.value)
+    input.value = ''
+})
 
-}{
+document.getElementById('readSesssion').addEventListener('click', function () {
+    const info = sessionStorage.getItem('info')
+    const input = document.getElementById('session')
+    input.value = info
 
+})
+
+document.getElementById('localBtn').addEventListener('click', function () {
+    const input = document.getElementById('local')
+    localStorage.setItem('Arm:', input.value)
+
+})
+document.getElementById('readLocal').addEventListener('click', function () {
+    const input = document.getElementById('local')
+    const info = localStorage.getItem('Arm:')
+    input.value = info
+})
+
+
+document.getElementById('localBtn').addEventListener('click', function () {
+    const input = document.getElementById('cookie')
+    window.co
+
+})
+document.getElementById('cookieBtn').addEventListener('click', function () {
+    const input = document.getElementById('cookie')
+    const cookie = 'infoCookie=' + input.value + ';'
+    const expiration = 'expires' + new Date(2024, 10, 8) + ';'
+    const path = 'path=/;'
+    document.cookie = cookie + expiration + path
+    input.value = ''
 })
