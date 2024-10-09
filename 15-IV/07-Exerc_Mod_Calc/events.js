51364c7c46215f6b266e116d0bc2f1758f81a48e
Henrique Modena <modena3d@gmail.com>
2024-10-09 18:19:21 -0300

    aa

---
+++ 15-IV/07-Exerc_Mod_Calc/events.js
@@ -0,0 +1,25 @@
+import calculate from "./calculate.js"
+
+const input = document.querySelector('#input')
+
+function keydown(ev) {
+    const allowedKeys = ["(", ")", "/", "*", "-", "+", "9", "8", "7", "6", "5", "4", "3", "2", "1", "0", ".", "%", " "]
+    ev.preventDefault()
+    if (allowedKeys.includes(ev.key)) {
+      input.value += ev.key
+      return
+    }
+    if (ev.key === "Backspace") {
+      input.value = input.value.slice(0, -1)
+    }
+    if (ev.key === "Enter") {
+      calculate()
+    }
+  }
+
+  function clear() {
+    input.value = ""
+    input.focus()
+  }
+
+  export {keydown, clear}
