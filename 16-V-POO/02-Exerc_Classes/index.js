43a349a3b983b3877b0672733252e31fa44f6d60
Henrique Modena <modena3d@gmail.com>
2024-10-11 16:33:25 -0300

    exer classes

---
+++ 16-V-POO/02-Exerc_Classes/index.js
@@ -0,0 +1,18 @@
+// Treinando a Criação de Classes
+// Classe USER
+//1- um atributo fullname, atribuível na instanciação
+//2- um atributo email, atribuível na instanciação
+//3- um atributo password, atribuível na instanciação
+//4- um método login, que tem como parâmetros um email e uma senha e 
+// deve comparar esses parâmetros com o email e a senha do usuário e 
+// mostrar uma mensagem no console dizendo se o login foi bem sucedido ou não
+
+// Classe Product
+//5- um atributo name, atribuível na instanciação
+//6- um atributo description, atribuível na instanciação
+//7- um atributo price, atribuível na instanciação
+//8- um atributo inStock, inicializado sempre em 0
+//9- um método addToStock, que tem como parâmetro a quantidade a ser adicionada
+//  em estoque e deve somar essa quantidade à variável inStock
+//10- um método calculateDiscount, que tem como parâmetro a percentagem de desconto
+//  a ser aplicada e retorne o valor do preço com o desconto aplicado
