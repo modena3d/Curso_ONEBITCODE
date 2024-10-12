cd3e02e8a76d490997e72ed055cd9c13f58388a0
EmpresaFict <modena3d@gmail.com>
2024-10-12 01:36:06 -0300

    associacao

---
+++ 16-V-POO/03-associacao/Adress.js
@@ -0,0 +1,12 @@
+class Address{
+  constructor(street, number, neightborhood, city, state){
+    this.street
+    this.number
+    this.neightborhood
+    this.city
+    this.state
+  }
+  fullAdress(){
+    return 'Rua ${}'
+  }
+}
